4efc6f46e0233d5ded94af02dc4c1a45a537b484
ekin <ekinkarakoc1@gmail.com>
2023-11-05 05:40:46 +0300

    first commit

--- client/src/Filmler/FilmListesi.js
+++ client/src/Filmler/FilmListesi.js
@@ -1,27 +1,12 @@
-import React from 'react';
+import React from "react";
+import FilmCard from "./FilmCard";
 
 export default function FilmListesi(props) {
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <FilmDetayları key={movie.id} movie={movie} />
+      {props.movies.map((movie) => (
+        <FilmCard key={movie.id} movie={movie} />
       ))}
-    </div>
-  );
-  }
-
-function FilmDetayları(props) {
-  const { title, director, metascore } = props.movie;
-
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
     </div>
   );
 }
